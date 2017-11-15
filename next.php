8a7b400feac7e0fe63fc01f494a4583145ce3779
aashishshrestha5532 <aashishshrestha5532@gmail.com>
2017-11-15 17:56:38 +0545

    something changed

--- next.php
+++ next.php
@@ -4,9 +4,8 @@
   <title>Bulls Gym house</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
 
@@ -25,7 +24,7 @@
       <tr>
         <td>Default</td>
         <td>Defaultson</td>
-        <td>def@somemail.com</td>
+        <td>defasdsadsad@somemail.com</td>
       </tr>      
       <tr class="success">
         <td>Success</td>
