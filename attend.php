b5c7dde3f20c4fc1698a0478c93086845e7ba1b5
Talank <talankbaral@gmail.com>
2017-11-17 02:07:07 +0545

    few bugs solved

--- attend.php
+++ attend.php
@@ -1,7 +1,7 @@
 <?php
 include"connect.php";
 $user_id=$_GET['id'];
-date_default_timezone_set('GMT+5.45');
+date_default_timezone_set('Asia/Kathmandu');
 
  $date=date("Y/m/d");
 $time=date("h:i:s");
@@ -9,7 +9,7 @@ $time=date("h:i:s");
  $clock=date("a");
 
 
-$stmt="INSERT into attendance values ('',$user_id,'$date','$time','$clock')";
+$stmt="INSERT into attendance(user_id,date,time,clock) values ($user_id,'$date','$time','$clock')";
 $result=mysqli_query($conn,$stmt);
 if($result){
   header("location:search.html");
