e38b6ca99be55f08a0f4f0586d3df78e3a829daf
susonthapa <susuthapa19961227@gmail.com>
2017-12-07 15:31:24 +0545

    login design,Image resize function

--- process/form-process.php
+++ process/form-process.php
@@ -1,5 +1,6 @@
 <?php
 
+include "functions.php";
  $firstname=$_POST['firstname'];
 
  $lastname=$_POST['lastname'];
@@ -29,6 +30,7 @@ else{
   date_add($date,date_interval_create_from_date_string("12 months"));
 }
 $plan=date_format($date,"Y-m-d");
+echo "Date Calculated:".$plan;
 
  include"../pages/connect.php";
  $stmt="INSERT into users values('','$firstname','$lastname','$plan','$issue','$image')";
@@ -53,7 +55,7 @@ $plan=date_format($date,"Y-m-d");
 $target_dir = "../Images/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$imageFileType =strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -71,10 +73,10 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+/*if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
-}
+}*/
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
@@ -86,7 +88,10 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	if($imageFileType=="jpg"||$imageFileType=="jpeg")
+		$imageFileType='jpeg';
+	
+    if (resizeImage(500,$_FILES["fileToUpload"]["tmp_name"],$target_file,$imageFileType)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
