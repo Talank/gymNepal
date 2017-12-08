55f7a2a83c7c03e5ddbe9b59aa86b2521c325880
susonthapa <susuthapa19961227@gmail.com>
2017-12-08 19:14:18 +0545

    Slight Design Change

--- process/sql2.php
+++ process/sql2.php
@@ -1,18 +1,10 @@
 
-<!DOCTYPE html>
-<html>
-<head>
   <!-- <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7/css/bootstrap.css"> -->
 
-  <link rel="stylesheet" type="text/css" href="../css/table.css">
-
-  <title></title>
-</head>
-<body>
-
+  
  <!--  <script src="js/jquery.slim.min.js"></script>
   <script src="js/thether.min.js"></script>
   <script src="js/bootstrap.min.js"></script> -->
@@ -26,9 +18,7 @@
 
       include"../pages/connect.php";
       $stmt=" SELECT *,datediff(duedate,now()) as count from `users`,`information`,`status`  where (users.firstname like '%$name%' ||users.user_id ='$name') and information.user_id=users.user_id and users.user_id=status.user_id";
-    //  $stmt="select * from users where firstname like '%$name'";
 	  $result=mysqli_query($conn,$stmt);
-	//	$todayDate=date_create(date('y-m-d',time()));
       $num=mysqli_num_rows($result);
 		echo "Number:".$num;
       $suffix=($num!=1 && $num!=0)?'s':'';
@@ -36,13 +26,18 @@
 		if($num>0) {
 			// echo"<div class='container'>";
 
-            echo"<table id=t1><tr><th>Reg no.</th>
-                <th>Dp</th>
-                <th>Name</th>
-                <th>address</th>
-                <th>R.day</th>
-                <th>Status</th>
-                <th>Action</tr>";
+            echo'<div class="table-responsive"><table id=t1 class="table table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th scope="col">Reg no.</th>
+						<th scope="col">Dp</th>
+						<th scope="col">Name</th>
+						<th scope="col">address</th>
+						<th scope="col">R.day</th>
+						<th scope="col">Status</th>
+						<th scope="col">Action</th>
+					</tr>
+				</thead><tbody>';
 
             //eliminating fields while displaying in mobile 
             // echo "<table id=t1InMedia><tr><th>Reg no.</th>
@@ -54,22 +49,21 @@
      {
       //  $count=date_diff($row['duedate'],$todayDate);
         echo"<tr>";
-          echo "<td>" .$row['user_id']."</td>";
-          echo "<td><img src=../Images/$row[picture] height=70 width=70 style=padding:10px;></td>";
-          echo "<td>".$row['firstname']." ".$row['lastname']."</td>";
-          echo "<td >".$row['temporay_address']."</td>";
+        echo '<th scope="row">' .$row['user_id']."</th>";
+        echo "<td><img src=../Images/$row[picture] height=70 width=70 style=padding:10px;></td>";
+        echo "<td>".$row['firstname']." ".$row['lastname']."</td>";
+        echo "<td >".$row['temporay_address']."</td>";
 
-          if($row['count']>0){
-           echo "<td id=color>".$row['count']."</td>";
-          }
-          else{
+        if($row['count']>0){
+			echo "<td id=color>".$row['count']."</td>";
+         } else{
             echo '<td style="color: red;">finished</td>';
-          }
+        }
            
-          $message=($row['status']!=0)?'<img src=../Images/online.png width=30 height=28':'<img src=../Images/offline.png  width=23  height=22';
+        $message=($row['status']!=0)?'<img src=../Images/online.png width=30 height=28':'<img src=../Images/offline.png  width=23  height=22';
            
-          echo"<td>".$message."</td>";
-          echo "<td>
+        echo"<td>".$message."</td>";
+        echo "<td>
                   <a href=renew.php?id=$row[user_id]>
                     <button class='edit_btn'>
                       Edit
@@ -80,7 +74,7 @@
                     echo"
                       <a href=attend.php?id=$row[user_id]>
                         <button class='tick_mark'>
-                          <img src=../Images/tick.png id=img>
+                          <img src=../Images/tick.png width=30 height=28 id=img>
                         </button>
                       </a>";
                   }
@@ -112,7 +106,7 @@
 
       }
 
-      echo"</table>";
+      echo"</tbody></table></div>";
 		}
 
             
@@ -131,5 +125,3 @@
   }
 ?>
 
-</body>
-</html>
